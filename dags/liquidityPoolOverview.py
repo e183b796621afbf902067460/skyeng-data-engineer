6364ededefff8ee2aa8df794a3ebae9ca03201a1
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-08-26 15:35:24 +0300

    Chnaged creds

--- dags/liquidityPoolOverview.py
+++ dags/liquidityPoolOverview.py
@@ -19,10 +19,10 @@ def liquidityPoolOverview():
 
         from sqlalchemy import create_engine
 
-        db_address = os.getenv('DB_ADDRESS', 'localhost')
-        db_user = os.getenv('DB_USER', 'username')
-        db_password = quote_plus(os.getenv('DB_PASSWORD', '111222'))
-        db_name = os.getenv('DB_NAME', 'dwh')
+        db_address = os.getenv('DB_ADDRESS', '')
+        db_user = os.getenv('DB_USER', '')
+        db_password = quote_plus(os.getenv('DB_PASSWORD', ''))
+        db_name = os.getenv('DB_NAME', '')
 
         db_url = f'postgresql://{db_user}:{db_password}@{db_address}/{db_name}'
         db_engine = create_engine(db_url)
