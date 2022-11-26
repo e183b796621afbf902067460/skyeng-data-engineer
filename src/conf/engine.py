96165a55c5e762d57d2c583d104fc2ac28ccf0a0
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-11-26 20:14:08 +0500

    Updated engine

--- src/conf/engine.py
+++ src/conf/engine.py
@@ -1,7 +1,12 @@
 import os
 from urllib.parse import quote_plus
 from sqlalchemy import create_engine
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
+# Uncomment after update
+# FUND_MICROSERVICE_URI = PostgresHook(postgres_conn_id='FUND_MICROSERVICE_CONN').get_uri()
+# POOL_MICROSERVICE_URI = PostgresHook(postgres_conn_id='POOL_MICROSERVICE_CONN').get_uri()
+# ALLOCATION_MICROSERVICE_URI = PostgresHook(postgres_conn_id='ALLOCATION_MICROSERVICE_CONN').get_uri()
 
 db_address = os.getenv('DB_ADDRESS', '')
 db_user = os.getenv('DB_USER', '')
