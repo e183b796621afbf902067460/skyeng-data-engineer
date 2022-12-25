4cc0c6843301b3fff11300dadc1087fe0f8de143
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-25 16:59:43 +0500

    Updated DWHSettings

--- dwh/cfg/engine.py
+++ dwh/cfg/engine.py
@@ -10,10 +10,10 @@ from head.interfaces.db.settings.interface import ISettings
 
 
 class DWHSettings(ISettings):
-    DB_ADDRESS = os.getenv('DB_ADDRESS', 'localhost')
-    DB_USER = os.getenv('DB_USER', 'default')
-    DB_PASSWORD = quote_plus(os.getenv('DB_PASSWORD', '111222'))
-    DB_NAME = os.getenv('DB_NAME', 'clickhouse')
+    DB_ADDRESS = os.getenv('DB_ADDRESS', '')
+    DB_USER = os.getenv('DB_USER', '')
+    DB_PASSWORD = quote_plus(os.getenv('DB_PASSWORD', ''))
+    DB_NAME = os.getenv('DB_NAME', '')
 
     DB_URL = f'clickhouse://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}/{DB_NAME}'
 
