49573bd84a14d25291b457b2076d06203eb86bcb
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-02-06 02:18:31 +0500

    Updated docker-compose

--- c3d3/c3d3/schedules/wallet_balances/erc20/schedules.py
+++ c3d3/c3d3/schedules/wallet_balances/erc20/schedules.py
@@ -3,4 +3,8 @@ from dagster import ScheduleDefinition
 from c3d3.jobs.wallet_balances.erc20.jobs import dag
 
 
-every_10th_minute = ScheduleDefinition(job=dag, cron_schedule="*/1 * * * *")
+every_10th_minute = ScheduleDefinition(
+    name='wallet_balances_erc20',
+    job=dag,
+    cron_schedule="*/1 * * * *"
+)
